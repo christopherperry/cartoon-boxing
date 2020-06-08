4b538d067a910f0474ac1a9c13e9c3fec9032ffc
Christopher Perry <mr.christopher.perry@gmail.com>
2020-06-07 17:09:13 -0700

    game start overlay

---
+++ Assets/Code/GameStartOverlay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameStartOverlay : MonoBehaviour
+{
+    public GameObject redIdleImage;
+    public GameObject redPunchImage;
+
+    public GameObject blueIdleImage;
+    public GameObject bluePunchImage;
+
+    private void Awake()
+    {
+        redIdleImage.SetActive(true);
+        redPunchImage.SetActive(false);
+
+        blueIdleImage.SetActive(true);
+        bluePunchImage.SetActive(false);
+    }
+
+    public void OnBlueJoined()
+    {
+        blueIdleImage.SetActive(false);
+        bluePunchImage.SetActive(true);
+    }
+
+    public void OnRedJoined()
+    {
+        redIdleImage.SetActive(false);
+        redPunchImage.SetActive(true);
+    }
+}
